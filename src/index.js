04d2a182466aa4c9584e35b251b7c87705483433
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2023-08-07 20:13:54 +0700

    config sass

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+//static file
+app.use(express.static(path.join(__dirname,'public')))
 //http logger
 app.use(morgan('combined'))
 
