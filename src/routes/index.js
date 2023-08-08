cd83b6db5d14a285a71c8d64c02942aa02330e12
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2023-08-08 16:10:50 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const siteRouter = require('./site');
 function route(app) {
              app.use('/news', newsRouter);
 
-         app.use('/', siteRouter);
+       app.use('/', siteRouter);
 }
    module.exports = route;
